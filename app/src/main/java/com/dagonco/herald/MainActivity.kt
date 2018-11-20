9a171a6d5ef8fbb0d75a2e67232f73c91c05b8d7
David González Colorado <dagonco.dev@gmail.com>
2018-11-20 20:27:23 +0100

    Add Androidx and material dependencies to project

--- app/src/main/java/com/dagonco/herald/MainActivity.kt
+++ app/src/main/java/com/dagonco/herald/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.dagonco.herald
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
